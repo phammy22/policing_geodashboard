e1d8693dca9fa903c26485c50e55ea745af6120b
Logan Weidner <weidner1@uw.edu>
2023-03-04 22:51:29 -0800

    Change demo data to include 2013 to 2021 data

--- assets/data_cleaning.R
+++ assets/data_cleaning.R
@@ -6,10 +6,10 @@ library(sf)
 
 
 # Change these directories to where your data file is stored and to where you want to save plots; these are in a subdirectory of the R project where the .Rmd file is located.
-dataDir <- "./data"  #read data file dir
-dataDir2 <- "./plots"  #save plot files dir
+dataDir <- "./final_proj_data"  #read data file dir
+dataDir2 <- "./output"  #save plot files dir
 
-ddi <- read_ipums_ddi(file.path(dataDir,"usa_00001.xml")) # ACS download
+ddi <- read_ipums_ddi(file.path(dataDir,"usa_00007.xml")) # ACS download
 data <- read_ipums_micro(ddi, verbose=FALSE)
 
 # display first few rows of data file 
@@ -32,7 +32,7 @@ data$STATENAME <- as_factor(data$STATEFIP)
 varname <- as.data.frame(colnames(data))
 
 state_split <- data %>% 
-  group_by(STATENAME) %>% 
+  group_by(STATENAME, YEAR) %>% 
   mutate(StatePop=sum(PERWT)) %>% 
   ungroup()
 
@@ -43,11 +43,12 @@ state_split <- state_split %>%
     RACE == 2 ~ "Black", #People who are not Hispanic and are Black
     RACE >= 4 & RACE <= 6 ~ "Asian")) %>%  #People who are not Hispanic and are Asian
   filter(!is.na(Race)) %>% 
-  group_by(Race, STATENAME) %>% 
-  summarize(RaceProp=sum(PERWT)/StatePop) %>% 
-  distinct(STATENAME, RaceProp) %>% 
-  select(STATENAME, Race, RaceProp) %>% 
+  group_by(Race, STATENAME, YEAR) %>% 
+  mutate(RaceProp=sum(PERWT)/StatePop) %>% 
+  distinct(STATENAME, RaceProp, YEAR) %>% 
+  select(STATENAME, Race, RaceProp, YEAR) %>% 
   pivot_wider(names_from = Race, 
-              values_from = RaceProp,
-              )
+              values_from = RaceProp)
+
+write.csv(state_split, file=file.path(dataDir2, "ACS_Demographics_2013-21.csv"), row.names=FALSE)
   
