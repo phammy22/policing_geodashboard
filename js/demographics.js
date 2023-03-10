6ebe9cb2c2c6b4a3bb647e52bc6c982f3459fd53
kvpoch <kathypoch11@gmail.com>
2023-03-09 18:56:27 -0800

    hover attempt

--- js/demographics.js
+++ js/demographics.js
@@ -5,55 +5,18 @@ let map = new mapboxgl.Map({
     container: 'map', //container id
     style: 'mapbox://styles/phammy22/cles4fq22002601qghfyvqfpq', //map style
     center: [-95.7129, 39.7], //starting coordinates
-    zoom: 3.5 //starting zoom level
+    zoom: 4 //starting zoom level
 });
-/*
-// Load the GeoJSON data
-map.on('load', function () {
-    map.addSource('states', {
-      type: 'geojson',
-      data: 'assets/demographics.geojson'
-    });
-  
-    // Create a layer to display the state polygons
-    map.addLayer({
-      id: 'states-layer',
-      type: 'fill',
-      source: 'states',
-      paint: {
-        'fill-color': '#f2f0f7',
-        'fill-opacity': 0.8
-      }
-    });
-  
-    // Add a click event listener to the state polygons
-    map.on('click', 'states-layer', function (e) {
-      var stateName = e.features[0].properties.STATE;
-      var whitePercentage = e.features[0].properties.White;
-      var blackPercentage = e.features[0].properties.Black;
-      var latinePercentage = e.features[0].properties.Latine;
-      var asianPercentage = e.features[0].properties.Asian;
-  
-      // Display the racial breakdown in a popup
-      new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML('<h3>' + stateName + '</h3>' +
-                 '<p>White: ' + whitePercentage + '%</p>' +
-                 '<p>Black: ' + blackPercentage + '%</p>' +
-                 '<p>Latine: ' + latinePercentage + '%</p>' +
-                 '<p>Asian: ' + asianPercentage + '%</p>')
-        .addTo(map);
-    });
-  });
 
-
-*/
+async function geojsonFetch() {
+  let response = await fetch('assets/covid-rates.json');
+  let states = await response.json();
   
 map.on('load', function() {
   // Add a GeoJSON source containing your data
   map.addSource('states', {
     'type': 'geojson',
-    'data': 'assets/demographics.geojson'
+    'data': 'states'
   });
 
 
@@ -62,29 +25,9 @@ map.on('load', function() {
     console.log('Data loaded successfully!');
   });
   
-  // Add a layer for your data
   /*
   map.addLayer({
-    'id': 'states-layer',
-    'type': 'fill',
-    'source': 'states',
-    'paint': {
-      'fill-color': [
-        'interpolate',
-        ['linear'],
-        ['get', 'STATE'],
-        0, '#ffffcc',
-        0.2, '#c2e699',
-        0.4, '#78c679',
-        0.6, '#31a354',
-        0.8, '#006837'
-      ],
-      'fill-opacity': 0.7
-    }
-  });
-  */
-  map.addLayer({
-    'id': 'state-points',
+    'id': 'state-layer',
     'type': 'circle',
     'source': 'states',
     'paint': {
@@ -102,19 +45,88 @@ map.on('load', function() {
         'circle-stroke-color': 'white'
     }
 });
+*/
+
+map.addLayer({
+  'id': 'states-layer',
+  'type': 'fill',
+  'source': 'states',
+  'paint': {
+      'fill-color': [
+          'match',
+          ['get', 'STATE'],
+          'Alabama', '#FEB24C',
+  'Alaska', '#FD8D3C',
+  'Arizona', '#FC4E2A',
+  'Arkansas', '#E31A1C',
+  'California', '#BD0026',
+  'Colorado', '#800026',
+  'Connecticut', '#BD0026',
+  'Delaware', '#800026',
+  'Florida', '#FEB24C',
+  'Georgia', '#FC4E2A',
+  'Hawaii', '#FD8D3C',
+  'Idaho', '#E31A1C',
+  'Illinois', '#BD0026',
+  'Indiana', '#800026',
+  'Iowa', '#BD0026',
+  'Kansas', '#800026',
+  'Kentucky', '#FEB24C',
+  'Louisiana', '#FC4E2A',
+  'Maine','#FD8D3C',
+  'Maryland','#E31A1C',
+  'Massachusetts', '#BD0026',
+  'Michigan', '#800026',
+  'Minnesota', '#BD0026',
+  'Mississippi', '#800026',
+  'Missouri', '#FEB24C',
+  'Montana', '#FC4E2A',
+  'Nebraska', '#FD8D3C',
+  'Nevada', '#E31A1C',
+  'New Hampshire', '#BD0026',
+  'New Jersey', '#800026',
+  'New Mexico', '#BD0026',
+  'New York', '#800026',
+  'North Carolina','#FEB24C',
+  'North Dakota','#FC4E2A',
+  'Ohio', '#FD8D3C',
+  'Oklahoma', '#E31A1C',
+  'Oregon', '#BD0026',
+  'Pennsylvania', '#800026',
+  'Rhode Island', '#BD0026',
+  'South Carolina','#800026',
+  'South Dakota', '#FEB24C',
+  'Tennessee', '#FC4E2A',
+  'Texas', '#FD8D3C',
+  'Utah', '#E31A1C',
+  'Vermont', '#BD0026',
+  'Virginia', '#800026',
+  'Washington', '#BD0026',
+  'West Virginia', '#800026',
+  'Wisconsin','#FEB24C',
+  'Wyoming', '#FC4E2A'
+      ],
+      'fill-opacity': 0.8
+  }
+});
+
+
   // Create a popup object for displaying state demographic data
   var popup = new mapboxgl.Popup({
     closeButton: false,
-    closeOnClick: true
+    closeOnClick: false
   });
 
-  // Create an event listener for the 'click' event on the states-layer
-  map.on('click', 'state-points', function(e) {
-    var state = e.features[0].properties.STATE;
-    var white = Math.round(e.features[0].properties.White *100);
+
+  map.on('mousemove', ({point}) => {
+    const state = map.queryRenderedFeatures(point, {
+        layers: ['states_layer']
+    });
+    //var state = e.features[0].properties.STATE;
+    var white = Math.round(e.features[0].properties.White * 100);
     var black = Math.round(e.features[0].properties.Black * 100);
-    var latine = Math.round(e.features[0].properties.Latine*100);
-    var asian = Math.round(e.features[0].properties.Asian*100);
+    var latine = Math.round(e.features[0].properties.Latine * 100);
+    var asian = Math.round(e.features[0].properties.Asian * 100);
 
     // Set the popup HTML to display the state demographic data
     popup.setHTML('<h3>' + state + '</h3>' +
@@ -125,16 +137,16 @@ map.on('load', function() {
 
     // Set the popup location and open it
     popup.setLngLat(e.lngLat).addTo(map);
-  });
 
-  // Change the cursor to a pointer when the mouse is over the states-layer
-  map.on('mouseenter', 'state-points', function() {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change the cursor back to the default when the mouse leaves the states-layer
-  map.on('mouseleave', 'state-points', function() {
-    map.getCanvas().style.cursor = '';
-  });
+    
+   
 });
 
+  // Remove the popup when the mouse leaves the states-layer
+  map.on('mouseleave', 'states-layer', function() {
+    popup.remove();
+  });
+
+});
+}
+geojsonFetch();
